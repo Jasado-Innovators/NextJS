7686665d03da562fa87ad3042f74b3ee319e9001
ARMAN DOYOGAN <nagoyod@gmail.com>
2024-09-07 14:06:44 +0800

    Next JS first

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+
+
+import ECommerce from "@/components/Dashboard/E-commerce";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { Metadata } from "next";
+import SignUp from "./auth/signup/page";
+export const metadata: Metadata = {
+  title:
+    "Document Tracking System",
+  description: "Document Tracking System",
+};
+
+export default function Home(...args: []) {
+  return (
+    <DefaultLayout>
+        <SignUp></SignUp>
+    </DefaultLayout> 
+
+
+  );
+}
---
+++ src/types/package.ts
@@ -0,0 +1,11 @@
+export type Package = {
+  CODE: string;
+  REQUISITIONER: string;
+  TITLE: string;
+  CONTENT: string;
+  TYPE: string;
+  FROM_OFFICE : string;
+  RELEASED_BY : string;
+  RELEASED_DATE : string;
+  STATUS: string;
+};
